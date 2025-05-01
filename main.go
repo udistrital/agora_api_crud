a8cb98c2896345790d2fa704a851c777cde18f3d
lud <luasanchezl@udistrital.edu.co>
2025-05-01 01:09:44 -0500

    devops: Configuración Health check

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	_ "github.com/lib/pq"
 	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/auditoria"
+	"github.com/udistrital/utils_oas/xray"
 )
 
 func init() {
@@ -21,7 +24,7 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 
-beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
 		AllowHeaders: []string{"Origin", "x-requested-with",
@@ -33,5 +36,8 @@ beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
+	xray.InitXRay()
+	apistatus.Init()
+	auditoria.InitMiddleware()
 	beego.Run()
 }
